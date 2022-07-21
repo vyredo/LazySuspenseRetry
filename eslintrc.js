fc1361723d8c4ec58191c68d27777f1b35713879
Vidy Alfredo <vidy.alfredo@helixleisure.com>
2022-07-21 10:32:24 +0800

    init dependency and package.json

---
+++ eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      tsconfigRootDir: __dirname,
+      project: ['./tsconfig.eslint.json'],
+    },
+    plugins: ['@typescript-eslint', 'json', 'html'],
+    extends: [
+      'react-app',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended',
+      'prettier/react',
+    ],
+    rules: {
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+    },
+  }
